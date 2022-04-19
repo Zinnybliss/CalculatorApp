5706002062630d226b6f13d8c7a65b43ad886e15
Zinnybliss <50361623+Zinnybliss@users.noreply.github.com>
2022-04-19 21:57:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// display() function
+function display(val){
+
+    document.getElementById('result').value += val
+
+    return val
+
+}
+// solve() function
+function solve(){
+
+    let x = document.getElementById('result').value
+
+    let y = eval(x);
+
+    document.getElementById('result').value = y
+
+    return y
+
+}
+// clearScreen()
+// This function is called when the user clicks on the C or the clear button. 
+
+function clearScreen(){
+
+    document.getElementById('result').value = ''
+
+}
+
